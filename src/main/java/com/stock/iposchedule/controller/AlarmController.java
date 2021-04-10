4a7831c9fa17da3e314846214a18b4b99d661580
DAVIN <davin325@gmail.com>
2021-04-11 01:06:55 +0900

    스케줄 설정

--- src/main/java/com/stock/iposchedule/controller/AlarmController.java
+++ src/main/java/com/stock/iposchedule/controller/AlarmController.java
@@ -1,9 +1,8 @@
 package com.stock.iposchedule.controller;
 
 import com.stock.iposchedule.service.AlarmService;
-import com.stock.iposchedule.service.CrawlService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +14,11 @@ public class AlarmController {
     AlarmService alarmService;
 
 
+    @Scheduled(cron = "0 0 22 * * SUN")
+    @Scheduled(cron = "0 0 9 * * MON-FRI")
     @GetMapping("send")
     @ResponseBody
-    public String logDemo() {
+    public void sendBot() {
         alarmService.alarm();
-
-        return "TEST";
     }
 }
