7743f9201088b2a051417a779d1531060dac088f
DAVIN <davin325@gmail.com>
2021-04-11 12:47:17 +0900

    텔레그램 서비스 추가

--- src/main/java/com/stock/iposchedule/service/ChatService.java
+++ src/main/java/com/stock/iposchedule/service/ChatService.java
@@ -15,29 +15,44 @@ public class ChatService {
     public void slack(List<IPOinfo> sendList, String today) {
         String url = ""; // 사용할 슬랙의 Webhook URL 넣기
         RestTemplate restTemplate = new RestTemplate();
-        Map<String,Object> request = new HashMap<String,Object>();
+        Map<String, Object> request = new HashMap<String, Object>();
         String message = "";
-        if("일요일".equals(today)) {
-                message +="다음주 공모주 일정입니다.\n\n";
-        }else {
-            message +="오늘의 공모주 일정입니다.\n\n";
-        }
-            for(int i=0;i<sendList.size();i++) {
-                message +="공모주일정: "+sendList.get(i).getDate()+"\n";
-                message +="종목명: "+sendList.get(i).getCompany()+"\n";
-                message +="확정공모가: "+sendList.get(i).getFinalPrice()+"\n";
-                message +="희망공모가: "+sendList.get(i).getHopePrice()+"\n";
-                message +="주간사: "+sendList.get(i).getStockCompanys()+"\n";
-                message +="\n";
-            }
+        message = getString(sendList, today, message);
 
         request.put("username", "slackbot");
         request.put("text", message);
-        HttpEntity<Map<String,Object>> entity = new HttpEntity<Map<String,Object>>(request);
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<Map<String, Object>>(request);
         restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
     }
 
     public void telegram(List<IPOinfo> sendList, String today) {
+        String Token = "";
+        String chat_id = "";
+        String message = "";
+        String url = "https://api.telegram.org/bot" + Token + "/sendmessage?chat_id=" + chat_id + "&text=";
+        RestTemplate restTemplate = new RestTemplate();
+        Map<String, Object> request = new HashMap<String, Object>();
 
+        message = getString(sendList, today, message);
+        url += message;
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<Map<String, Object>>(request);
+        restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+    }
+
+    private String getString(List<IPOinfo> sendList, String today, String message) {
+        if ("일요일".equals(today)) {
+            message += "다음주 공모주 일정입니다.\n\n";
+        } else {
+            message += "오늘의 공모주 일정입니다.\n\n";
+        }
+        for (int i = 0; i < sendList.size(); i++) {
+            message += "공모주일정: " + sendList.get(i).getDate() + "\n";
+            message += "종목명: " + sendList.get(i).getCompany() + "\n";
+            message += "확정공모가: " + sendList.get(i).getFinalPrice() + "\n";
+            message += "희망공모가: " + sendList.get(i).getHopePrice() + "\n";
+            message += "주간사: " + sendList.get(i).getStockCompanys() + "\n";
+            message += "\n";
+        }
+        return message;
     }
 }
