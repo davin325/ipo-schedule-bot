82d56416f321a71e6e74f6a3fdfa541ee3254997
DAVIN <davin325@gmail.com>
2021-04-11 12:47:45 +0900

    배포시 타임존 셋팅 추가

--- src/main/java/com/stock/iposchedule/IpoScheduleApplication.java
+++ src/main/java/com/stock/iposchedule/IpoScheduleApplication.java
@@ -3,8 +3,15 @@ package com.stock.iposchedule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class IpoScheduleApplication {
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(IpoScheduleApplication.class, args);
